cf089cd9067ca8f31ed0dd2fc03057c9b6db4154
Michał Staniszewski <stanisz96@gmail.com>
2020-11-13 13:33:33 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DivineMonad.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
