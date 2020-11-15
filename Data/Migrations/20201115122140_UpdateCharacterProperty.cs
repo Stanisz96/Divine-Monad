dc72c8860af469d02eaf8eca4ab9c5ad72b5e5b5
Michał Staniszewski <stanisz96@gmail.com>
2020-11-15 13:27:52 +0100

    Update Character Model property

---
+++ Data/Migrations/20201115122140_UpdateCharacterProperty.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DivineMonad.Data.Migrations
+{
+    public partial class UpdateCharacterProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_CharactersBaseStats_CBStatsID",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "CStatsId",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "CBStatsID",
+                table: "Characters",
+                newName: "CBStatsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Characters_CBStatsID",
+                table: "Characters",
+                newName: "IX_Characters_CBStatsId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CBStatsId",
+                table: "Characters",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_CharactersBaseStats_CBStatsId",
+                table: "Characters",
+                column: "CBStatsId",
+                principalTable: "CharactersBaseStats",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_CharactersBaseStats_CBStatsId",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "CBStatsId",
+                table: "Characters",
+                newName: "CBStatsID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Characters_CBStatsId",
+                table: "Characters",
+                newName: "IX_Characters_CBStatsID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CBStatsID",
+                table: "Characters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CStatsId",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_CharactersBaseStats_CBStatsID",
+                table: "Characters",
+                column: "CBStatsID",
+                principalTable: "CharactersBaseStats",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -10,7 +10,7 @@ namespace DivineMonad.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string UserId { get; set; }
-        public int CStatsId { get; set; }
+        public int CBStatsId { get; set; }
         public CharacterBaseStats CBStats { get; set; }
         public int GStatsId { get; set; }
         public GameStats GStats { get; set; }
