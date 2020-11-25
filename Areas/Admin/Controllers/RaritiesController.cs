22fd1178ee2af0856dc25f77643ee739c09a701a
Michał Staniszewski <stanisz96@gmail.com>
2020-11-25 22:56:21 +0100

    Update Admin authorization in Admin Area

--- Areas/Admin/Controllers/RaritiesController.cs
+++ Areas/Admin/Controllers/RaritiesController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DivineMonad.Data;
 using DivineMonad.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DivineMonad.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class RaritiesController : Controller
     {
         private readonly ApplicationDbContext _context;
