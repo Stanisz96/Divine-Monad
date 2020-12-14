5f512ecd6df14441ac045ad66beb7ef3b1d165d9
Michał Staniszewski <stanisz96@gmail.com>
2020-12-14 22:25:20 +0100

    update nav bars and load user characters in characters view

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DivineMonad.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace DivineMonad.Controllers
 {
+    [Authorize(Policy = "user")]
     public class CharactersController : Controller
     {
-        
-        [Authorize(Policy = "user")]
-        public IActionResult Index()
+
+        private readonly ApplicationDbContext _context;
+
+        public CharactersController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userName = User.FindFirstValue(ClaimTypes.Name);
+            var characters = _context.Characters.Where(c => c.UserId == userId).Include(c => c.CBStats);
+
+            ViewData["userId"] = userId;
+            ViewData["userName"] = userName;
+
+            return View(await characters.ToListAsync());
         }
     }
 }
