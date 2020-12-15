f970a03f21ad35205688d9416821f9d6c6bffa0f
Michał Staniszewski <stanisz96@gmail.com>
2020-12-15 11:31:59 +0100

    initialize files

---
+++ Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivineMonad.Controllers
+{
+    [Authorize(Policy = "user")]
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ManageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DivineMonad.Controllers
+{
+    [Authorize(Policy = "user")]
+    [Route("Characters/Manage/")]
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
