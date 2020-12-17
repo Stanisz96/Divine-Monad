1b07e69bf0fbf58e94c169ee30815e7abd64b32d
Michał Staniszewski <stanisz96@gmail.com>
2020-12-17 17:03:02 +0100

    Move ReloadComponent function to main js file for cleaner code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function ReloadComponent(componentId, action, controller, data) {
+    $.ajax({
+        url: `/${controller}/${action}`,
+        data: data,
+        success: function (data) { $("#" + componentId).html(data); }
+    });
+};
