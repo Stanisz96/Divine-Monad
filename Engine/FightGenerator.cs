ce6caebdf38c10cdd704b1fc2becaba958018585
Michał Staniszewski <stanisz96@gmail.com>
2020-12-20 17:39:01 +0100

    Add new methods to Fight generator

--- Engine/FightGenerator.cs
+++ Engine/FightGenerator.cs
@@ -9,10 +9,21 @@ namespace DivineMonad.Engine
     public class FightGenerator
     {
         Random rand;
-        public string HPInfo { get; set; }
         public AdvanceStats PlayerStats { get; set; }
         public AdvanceStats OpponentStats { get; set; }
+        private AdvanceStats Attacker { get; set; }
+        private AdvanceStats Defender { get; set; }
         public RaportGenerator Raport { get; set; }
+        public int AttackerId { get; set; }
+        public int DefenderId { get; set; }
+        public int AttackerHp { get; set; }
+        public int DefenderHp { get; set; }
+        public bool IsCrit { get; set; }
+        public bool IsBlock { get; set; }
+        public bool IsMiss { get; set; }
+        public int Damage { get; set; }
+        public int Receive { get; set; }
+
 
         public FightGenerator(AdvanceStats playerStats, AdvanceStats opponentStats)
         {
@@ -23,23 +34,85 @@ namespace DivineMonad.Engine
 
         public void GenerateFight()
         {
-            HPInfo = "HP: " + PlayerStats.HitPoints.ToString() + "\n";
-            Action<AdvanceStats, AdvanceStats> fightAction = new Action<AdvanceStats, AdvanceStats>(DealDmgToPlayer);
-            fightAction += DealDmgToPlayer;
+            Action fightAction = new Action(SetMainRaportProps);
+            fightAction += SetWhoStart;
+            fightAction += SetStartHp;
 
-            fightAction(PlayerStats, OpponentStats);
+            for (int i = 1; i <= 5; i++)
+            {
+                fightAction += UpdateBonuses;
+                fightAction += UpdateDamage;
+            }
+
+            fightAction();
         }
 
-        private void DealDmgToPlayer(AdvanceStats player, AdvanceStats opponent)
+/*        private void DealDmgToPlayer(AdvanceStats player, AdvanceStats opponent)
         {
             player.HitPoints += rand.Next(0, 10) - 4;
             HPInfo += "HP: " + opponent.HitPoints + "\n";
+        }*/
+
+        private void SetMainRaportProps()
+        {
+            Raport.IsPvp = OpponentStats.IsPlayer;
+            Raport.Player = new Player() { ID = PlayerStats.CharacterId };
+            Raport.Opponent = new Opponent() { ID = OpponentStats.CharacterId };
+            Raport.Rounds = new List<Round>();
         }
 
-        private void SetMainRaportProps(AdvanceStats player, AdvanceStats opponent)
+        private void SetWhoStart()
         {
-            Raport.IsPvp = opponent.IsPlayer;
-            Raport.Player = new Player() { ID = player.CharacterId };
+            if (PlayerStats.Speed >= OpponentStats.Speed)
+            {
+                AttackerId = PlayerStats.CharacterId;
+                Attacker = PlayerStats;
+                DefenderId = OpponentStats.CharacterId;
+                Defender = OpponentStats;
+            }
+            else
+            {
+                AttackerId = OpponentStats.CharacterId;
+                Attacker = OpponentStats;
+                DefenderId = PlayerStats.CharacterId;
+                Defender = PlayerStats;
+            }
+        }
+
+        private void UpdateBonuses()
+        { 
+
+            if (rand.NextDouble() <= Attacker.CritPr) IsCrit = true;
+            else IsCrit = false;
+
+            if (rand.NextDouble() <= Defender.DodgePr) IsMiss = true;
+            else IsMiss = false;
+
+            if (rand.NextDouble() <= Defender.BlockPr) IsBlock = true;
+            else IsBlock = false;
+        }
+
+        private void SetStartHp()
+        {
+            AttackerHp = Attacker.HitPoints;
+            DefenderHp = Defender.HitPoints;
+        }
+
+        private void UpdateDamage()
+        {
+            Damage = rand.Next(Attacker.AttackMin, Attacker.AttackMax);
+            if (IsCrit)
+            {
+                Damage = (int)(Damage * 1.5);
+            }
+        }
+
+        private void UpdateReceiveDamage()
+        {
+            if(IsMiss)
+            {
+                Receive = 0;
+            }
         }
     }
 }
